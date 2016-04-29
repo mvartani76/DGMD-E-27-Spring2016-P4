c8ebe8930610af63379368ce80d95b429dbc1949
mvartani76 <mike.vartanian@gmail.com>
2016-04-29 14:42:17 -0400

    Initial attempt at starting a css4 grid layout. Looks like it is working for index.php. Removed the rows. Changed main to content to be consistent with css4 grid setup names.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 		<!-- Replaced header with row div as this seems to allow the header image to stay
 			 adjacent to the menu when scaling to the different screen sizes. -->
-		<div class="row">
+		<div class="header">
 			<img data-interchange="[img/sog-entrance-500.jpg, small], 
 				[img/sog-entrance-800.jpg, medium], 
 				[img/sog-entrance-1000.jpg, large], 
@@ -24,12 +24,13 @@
 			<noscript><img src="img/sog-entrance-800.jpg.jpg"></noscript>
 		</div>
 
+		<div class="css4menu">
 		<div class="title-bar" data-responsive-toggle="nav-menu" data-hide-for="medium">
 			<button class="menu-icon" type="button" data-toggle></button>
 			<div class="title-bar-title">Menu</div>
 		</div>
 
-		<div class="row">
+
 			<div class="top-bar" id="nav-menu">
 				<ul class=" medium-horizontal vertical dropdown menu" data-responsive-menu="accordion medium-dropdown">
 					<li class="active"><a href="./index.php">Home</a></li>
@@ -47,8 +48,7 @@
 				</ul>
 			</div>
 		</div>
-		<div class="row">
-			<main class="xlarge-9 large-9 medium-9 columns">
+		<div class="content">
 				<h2>Welcome to the Shores of Glenwood Neighborhood Association Website!</h2>
 				
 				<!-- Using interchange, load different pictures depending on the width of the screen
@@ -74,9 +74,8 @@
 				</div>
 
 				<p>We are an active community of families, young couples, empty-nesters, and other lively people who enjoy a peaceful atmosphere and close proximity to city attractions and water recreation areas.</p>
-
-			</main>
-			<div class="outside-w-wrapper xlarge-3 large-3 medium-3 columns hide-for-small-only">
+		</div>
+			<div class="outside-w-wrapper">
 				<h3>Local Time &amp; Weather</h3>
 				<p>
 					<?php
@@ -126,11 +125,9 @@
 				</div>
 			</div>			
 		</div>
-	</div>
-	<div class="row">
-	    <footer class="xlarge-12 large-12 medium-12 columns">
+		<div class="footer">
 	    	<p>&copy; 2016 Shores of Glenwood Neighborhood Association </p>
-	    </footer>
+		</div>
 	</div>
 
 	<!-- Specific code for configuring the openweather data to the subdivision location -->
